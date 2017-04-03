24cf51821431608e2590120529fc51a63d3a6e2c
aiasfina <aiasfina@gmail.com>
2017-04-03 15:40:50 +0800

    Fixed

--- example/index.js
+++
@@ -1,9 +0,0 @@
-m.mount(document.querySelector('main'), {
-  view: vnode => m(infiniteList, {
-    rootTag: 'ol',
-    maxCursor: 3,
-    pullRefreshable: true,
-    item: (data, index) => m('li', data.title),
-    fetch: cursor => m.request('/example/data.json', {data: {page: cursor}})
-  })
-})
